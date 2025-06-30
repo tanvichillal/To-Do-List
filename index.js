ff1cacaa17efe9a24427c34135a04067e9507363
tanvichillal <chillaltanvi@gmail.com>
2025-06-30 16:18:51 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,39 @@
+    const input = document.getElementById("inputbox");
+    const listcontainer = document.getElementById("list-container");
+
+    function addtask(){
+        if(input.value === ''){
+            alert("You must write something");
+        }
+        else{
+            let li = document.createElement("li");
+            li.innerHTML = input.value;
+            listcontainer.appendChild(li);
+            let span = document.createElement("span");
+            span.innerHTML = "\u00d7";
+            li.appendChild(span);
+        }
+        input.value = "";
+        saveData()
+    }
+
+    listcontainer.addEventListener("click" , function(e){
+        if(e.target.tagName === "LI"){
+            e.target.classList.toggle("checked");
+            saveData()
+        }
+        else if(e.target.tagName === "SPAN"){
+            e.target.parentElement.remove();
+            saveData()
+        }
+    },false);
+
+    function saveData(){
+        localStorage.setItem("data" , listcontainer.innerHTML);
+    }
+
+    function showTask(){
+        listcontainer.innerHTML = localStorage.getItem("data");
+    }
+
+    showTask();
